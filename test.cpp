2261cf3f1935434d35f67fedd498a39ac94870db
CHIH-HSUAN,WANG <766steven@gmail.com>
2024-07-10 16:06:44 +0800

    creat file

---
+++ test.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    char name[11]; 
+    char department;
+
+    printf("Please input your name.... ");
+    scanf("%s", name);
+    getchar();
+
+    printf("Please select your department....(a:CS b:Bio c:Math) ");
+    scanf("%c", &department);
+
+}
