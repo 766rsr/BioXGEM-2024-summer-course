53f0e7b75f9ed9ac0fc046cf95f73d7151c2c51c
766rsr <766rsr@gmail.com>
2024-07-11 12:12:11 +0800

    新增20240710範例

---
+++ 20240710/Q1(cincout).cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <iomanip> 
+#include <string>
+using namespace std;
+
+int main() {
+    string name;
+    char department;
+    int test1, test2;
+    double average;
+
+    cout << "Please input your name... ";
+    cin >> name;
+    cout << "Please select your department... (a:CS b:Bio c:Math) ";
+    cin >> department;
+    cout << "Please input your score on test 1... ";
+    cin >> test1;
+    cout << "Please input your score on test 2... ";
+    cin >> test2;
+
+    average = float(test1 + test2) / 2;
+
+    cout << "+---------------------+----------+------+------+-------+" << endl;
+    cout << "|        Name         |Department|Test 1|Test 2|Average|" << endl;
+    cout << "+---------------------+----------+------+------+-------+" << endl;
+    cout << "|" << left << setw(21) << name
+         << "|" << right << setw(10) << department
+         << "|" << setw(6) << test1
+         << "|" << setw(6) << test2
+         << "|" << fixed << setprecision(2) << setw(7) << average << "|" << endl;
+    cout << "+---------------------+----------+------+------+-------+" << endl;
+
+    return 0;
+}
+
+
---
+++ 20240710/Q1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+int main() {
+    char name[11];
+	char department;
+    int test1, test2;
+    double average;
+
+    printf("Please input your name... ");
+    scanf("%s", name);
+    printf("Please select your department... (a:CS b:Bio c:Math) ");
+    
+	getchar();  
+	
+	scanf("%c", &department);
+    printf("Please input your score on test 1... ");
+    scanf("%d", &test1);
+    printf("Please input your score on test 2... ");
+    scanf("%d", &test2);
+
+    average = float(test1 + test2) / 2;
+
+    printf("+---------------------+----------+------+------+-------+\n");
+    printf("|        Name         |Department|Test 1|Test 2|Average|\n");
+    printf("+---------------------+----------+------+------+-------+\n");
+    printf("|%-21s|%10c|%6d|%6d|%7.2f|\n", name, department, test1, test2, average);
+    printf("+---------------------+----------+------+------+-------+\n");
+
+    return 0;
+}
+
+
